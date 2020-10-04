1b14e69aa9edbe76784396ebb1789fdf7190dafa
r7g49 <r7g.inbox@gmail.com>
2020-10-04 23:35:38 +0530

    Updates

---
+++ precache-manifest.bd92be52fff961ecc90029b1565b6bc8.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e83d93a58f054e3b3e7a2ad392f3e816",
+    "url": "/vipulsinghofficial/portfolio/index.html"
+  },
+  {
+    "revision": "b347ec67a303186e2597",
+    "url": "/vipulsinghofficial/portfolio/static/css/main.68e71262.chunk.css"
+  },
+  {
+    "revision": "390023c42d3566e0c203",
+    "url": "/vipulsinghofficial/portfolio/static/js/2.cff82c1f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/vipulsinghofficial/portfolio/static/js/2.cff82c1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b347ec67a303186e2597",
+    "url": "/vipulsinghofficial/portfolio/static/js/main.bc732474.chunk.js"
+  },
+  {
+    "revision": "eafbf443bf216778fb49",
+    "url": "/vipulsinghofficial/portfolio/static/js/runtime-main.da956f1d.js"
+  },
+  {
+    "revision": "8d41cf7ceff2834c469e66a991873790",
+    "url": "/vipulsinghofficial/portfolio/static/media/blueFolder.8d41cf7c.png"
+  },
+  {
+    "revision": "b4dd4c5e18b3c0a0c35018f37c2b2f94",
+    "url": "/vipulsinghofficial/portfolio/static/media/c.b4dd4c5e.png"
+  },
+  {
+    "revision": "fd45217ebbd97310d55c71c92c1f6b7c",
+    "url": "/vipulsinghofficial/portfolio/static/media/creativity-concept.fd45217e.jpg"
+  },
+  {
+    "revision": "5164d8ff545d887a376df7e1e19f7436",
+    "url": "/vipulsinghofficial/portfolio/static/media/css3.5164d8ff.png"
+  },
+  {
+    "revision": "ddafda7c4d93ffac704840dcf013e79c",
+    "url": "/vipulsinghofficial/portfolio/static/media/design-thinking.ddafda7c.jpg"
+  },
+  {
+    "revision": "652fdb659a681116811612e0b9e25354",
+    "url": "/vipulsinghofficial/portfolio/static/media/java.652fdb65.png"
+  },
+  {
+    "revision": "c2e0ad94286c2488ea3a1d76b7197669",
+    "url": "/vipulsinghofficial/portfolio/static/media/js.c2e0ad94.png"
+  },
+  {
+    "revision": "fc44a3bd7b32f9af39416011e0035b08",
+    "url": "/vipulsinghofficial/portfolio/static/media/mongodb.fc44a3bd.png"
+  },
+  {
+    "revision": "4c6c0929cc58f241847a744b2de75e2f",
+    "url": "/vipulsinghofficial/portfolio/static/media/node.4c6c0929.png"
+  },
+  {
+    "revision": "64eb742ef862ea06b1a86d4d9782c5ef",
+    "url": "/vipulsinghofficial/portfolio/static/media/postgres.64eb742e.png"
+  },
+  {
+    "revision": "22eeecf99b8d92726b4f3735be5473c0",
+    "url": "/vipulsinghofficial/portfolio/static/media/python.22eeecf9.png"
+  },
+  {
+    "revision": "2d2b0d56aeefb1b0f3025195cb2ff7b6",
+    "url": "/vipulsinghofficial/portfolio/static/media/react.2d2b0d56.png"
+  }
+]);
